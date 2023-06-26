35367c7a7e4e6373d5c519d8ef2b18a890de823a
Aarogya Yadav <aky104@student.bham.ac.uk>
2023-06-26 22:36:08 +0100

    implemented random problem chooser

--- chooser.py
+++ chooser.py
@@ -1,2 +1,22 @@
 import random
 
+topics = []
+problems = []
+
+
+with open("problems.txt") as problemFile:
+    for line in problemFile:
+        topics.append(line.split(':')[0])
+        problems.append(line.split(':')[1].split(','))
+
+    problems[len(problems)-1][len(problems[len(problems)-1])-1] = problems[len(problems)-1][len(problems[len(problems)-1])-1][:-1]
+
+
+topicChoice = random.choice(topics)
+topicNum = topics.index(topicChoice)
+
+problemChoice = random.choice(problems[topicNum])
+print(topicChoice)
+print(problemChoice[1:])
+
+
